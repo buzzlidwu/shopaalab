6bb5c05cdace523200ffec98fcd8b6c12f769c54
buzzlidmacpro <syuri3975@gmail.com>
2019-10-03 14:43:10 +0800

    fix lab3 error v2

--- lab3/script.js
+++ lab3/script.js
@@ -1,7 +1,7 @@
 window.onload = function(){
 	const send_btn = this.GetElement('send_btn')
 	const check_btn = this.GetElement('check_btn')
-	check_btn.addEventListener('onclick',()=>{this.check_status(check_btn,send_btn)})
+	check_btn.addEventListener('change',()=>{this.check_status(check_btn,send_btn)})
 }
 function check_status(check,send){
 	if(check.checked){
