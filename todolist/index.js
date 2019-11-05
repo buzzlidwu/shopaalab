f78dad3e221ba978d7e0ce570468b5168378cb5c
buzzlid <syuri3975@gmail.com>
2019-11-06 02:47:04 +0800

    todolisst

--- todolist/index.js
+++ todolist/index.js
@@ -1,9 +1,10 @@
-const range = (key,index,color)=>{
+const range = (key,index,color,status)=>{
   let arr = Array.from({length:index},(v,k)=>{
     let obj = {
       todo:[],
       day: k + key,
-      color: color
+      color: color,
+      side: status
     }
     return obj
   })
@@ -13,6 +14,7 @@ const now_date = new Date()
 let now_year = now_date.getFullYear()
 let now_month = now_date.getMonth()
 let todolist = {}
+let add_tag = [0,0]
 window.onload = ()=>{
   checkStatus()
   $('#year_status').text(`${now_year}年`)
@@ -42,6 +44,20 @@ $('#change_mm_left').click(()=>{
   console.log(todolist);
   
 })
+$('#add_todo').click(()=>{
+  let [month , id] = add_tag
+  let text = $('#todo_input').val()
+  if(text == '') return alert('新增代辦不能為空')
+  todolist[now_year][month][id].todo.push(text)
+  saveLocal(JSON.stringify(todolist))
+  createHTML(todolist[now_year][now_month])
+  $('#show_form').css('display','none')
+})
+
+$('#close_todo').click(()=>{
+  $('#show_form').css('display','none')
+})
+
 function changeView(){
   $('#year_status').text(`${now_year}年`)
   $('#month_status').text(`${now_month + 1}月`)
@@ -71,7 +87,7 @@ function createMMDD(yy,mm){
   let last_day = new Date(yy,mm,0).getDate()
   let day_len = new Date(yy,mm+1,0).getDate()
   
-  return [...range(last_day - first_day + 1, first_day,'#333333' ),...range(1,day_len,'red'),...range(1,42  - (day_len + first_day),'#333333')]
+  return [...range(last_day - first_day + 1, first_day,'#333333', 'per' ),...range(1,day_len,'red','now'),...range(1,42  - (day_len + first_day),'#333333','next')]
 }
 function localGET(){
   if( localStorage.getItem('_my_todolist') ) {
@@ -84,22 +100,61 @@ function saveLocal(obj){
 }
 
 function createHTML(arr){ 
-  todolist[2019][10][0].todo = [1,2,3]
   let htmlcode = ''
   for(let i in arr){
-    if(i % 7 == 0){
-      htmlcode += `<tr>`
-    }
-    htmlcode+= `<td class='table_days'data-order=${i}>
-    <span class='day_font' style='color:${arr[i].color}'>${arr[i].day}</span>` 
+    htmlcode+= `<div class='todo_context' data-order=${i}>
+    <div class='day_font' style='color:${arr[i].color}'>${arr[i].day}</div>` 
 
     for(let j of arr[i].todo){
       htmlcode+= `<li class='todo_item'>${j}</li>`
     }
-    htmlcode+= `</td>`
+    htmlcode+= `</div>`
   }
-  $('#table_body').html(htmlcode)
-  $('#table_body td').click(function(){
-    $this = $(this)
+  $('#js_install_todo').html(htmlcode)
+  $('#js_install_todo div').dblclick(function(){
+    showTable($(this).data('order'))
+  })
+}
+function showTable(id){
+  add_tag = [now_month,id]
+  let data = todolist[now_year][now_month][id]
+  let month = now_month
+  if(data.side == 'now'){
+    console.log('now');
+    month++
+  }
+  else if(data.side == 'next'){
+    month+=2
+  }
+  let date = `${now_year} / ${month} / ${data.day}`
+  let todo = data.todo
+
+  $('#data_bar').text(`選擇日期為 ： ${date}`)
+  if(todo.length > 0){
+    createTodoView(todo,id)
+  }
+  $('#todo_input').val('')
+  $('#show_form').css('display','flex')
+}
+function  createTodoView(todo,id){
+  let htmlcode = ''
+  if(todo.length > 0){
+    for(let i in todo){
+      if(i)
+      htmlcode+= `
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+      ${todo[i]}
+        <span class="badge badge-primary badge-pill" data-index='[${id},${i}]'>X</span>
+      </li>`
+    }
+  }
+  $('#todo_list').html(htmlcode)
+  $('#todo_list li span').dblclick(function(){
+    let data_pos = $(this).data('index')
+    let [index,item] = data_pos
+    todolist[now_year][now_month][index].todo.splice(item,1)
+    saveLocal(JSON.stringify(todolist))
+    createTodoView(todolist[now_year][now_month][index].todo,add_tag[1])
+    createHTML(todolist[now_year][now_month])
   })
 }
